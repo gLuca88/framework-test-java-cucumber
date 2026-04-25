ca345e3e9171b6542fa7a6f3f618c15eee367fd4
gLuca88 <gianluca.88corsi@gmail.com>
2026-04-25 16:40:57 +0200

    [FEAT](products): add product validation and improve product loading logic

--- src/main/java/com/gianluca/framework/pages/ProductsPage.java
+++ src/main/java/com/gianluca/framework/pages/ProductsPage.java
@@ -2,6 +2,9 @@ package com.gianluca.framework.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ProductsPage extends BasePage {
 
@@ -13,12 +16,19 @@ public class ProductsPage extends BasePage {
     }
 
     private By container_ProductsTile = By.className("title");
-    private By button_MenuHamburgher=By.id("react-burger-menu-btn");
-    private By button_LogOut=By.cssSelector("a[data-test='logout-sidebar-link']");
+    private By button_MenuHamburgher = By.id("react-burger-menu-btn");
+    private By button_LogOut = By.cssSelector("a[data-test='logout-sidebar-link']");
+
+    private By container_Product = By.className("inventory_item");
+    private By container_NameProduct = By.cssSelector("div[class='inventory_item_name ']");
+    private By container_Descricption = By.className("inventory_item_desc");
+    private By container_Price = By.className("inventory_item_price");
+
 
     public String getTitlePage() {
         return actions.getText(container_ProductsTile);
     }
+
 
     public boolean isPageProductsLoaded() {
         return actions.isDisplayed(container_ProductsTile)
@@ -26,9 +36,45 @@ public class ProductsPage extends BasePage {
                 && getTitlePageHtml().equalsIgnoreCase(EXPECTED_TITLE);
     }
 
-    public void executeLogout()  {
+    public void executeLogout() {
         actions.click(button_MenuHamburgher);
         actions.click(button_LogOut);
     }
+
+    public boolean areProductsLoaded(int nProducts) {
+
+        List<WebElement> products = actions.getElements(container_Product);
+        if (products == null || products.isEmpty()) {
+            return false;
+        }
+        if (products.size() < nProducts) {
+            return false;
+        }
+
+        return products.stream().allMatch(WebElement::isDisplayed);
+    }
+
+    public boolean areAllProductsComplete(){
+        List<WebElement> productList=actions.getElements(container_Product);
+
+        if(productList==null||productList.isEmpty()){
+            return false;
+        }
+
+        for(WebElement product: productList){
+
+            String nameP=product.findElement(container_NameProduct).getText();
+            String descriptionP=product.findElement(container_Descricption).getText();
+            String priceP=product.findElement(container_Price).getText();
+
+            if (nameP.trim().isEmpty() || descriptionP.trim().isEmpty() || priceP.trim().isEmpty()) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
 
 }
